885a8aff60966b2548ef588899429dba70ff0804
gzc-ceg <e1481823@u.nus.edu>
2026-01-28 16:28:47 +0800

    Add to List

--- src/main/java/Bruno.java
+++ src/main/java/Bruno.java
@@ -12,18 +12,30 @@ public class Bruno {
         System.out.println("    Hello! I am Bruno, your personl assistant!");
         System.out.println("    What can I do for you?");
 
-        Scanner scanner=new Scanner(System.in);
-        boolean isRunning=true;
-        while(isRunning){
+        Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        while (true) {
             String userInput = scanner.nextLine().trim();
-            if(userInput.equalsIgnoreCase("bye")){
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("    Bye! Hope to see you again!");
                 System.out.println("    Remember, Bruno is always here for you!");
                 break;
-            }else if(userInput.isEmpty()){
+            } else if (userInput.isEmpty()) {
                 System.out.println("    Please say something, I am listening...");
-            }else{
-                System.out.println("    "+userInput);
+            } else if (userInput.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println("    Your list is empty! Please first add some tasks!");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    "+(i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("    added: " + userInput);
             }
         }
         scanner.close();
