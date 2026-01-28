7153b52f3f073a83a8caa52178f948f1468378e9
gzc-ceg <e1481823@u.nus.edu>
2026-01-28 14:55:43 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Bruno.java
@@ -0,0 +1,14 @@
+public class Bruno {
+    public static void main(String[] args) {
+        String logo =
+                " ____  ____  _   _ _   _  ____  \n" +
+                        "| __ )|  _ \\| | | | \\ | |  _  |\n" +
+                        "|  _ \\| |_) | | | |  \\| | | | |\n" +
+                        "| |_) |  _ <| |_| | |\\  | |_| | \n" +
+                        "|____/|_| \\_\\\\___/|_| \\_| ____| \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I am Bruno, your personl assistance!");
+        System.out.println("What can I DO for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
