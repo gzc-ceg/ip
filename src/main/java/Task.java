76e01dc800cb8f773036136294366abad006ae08
gzc-ceg <e1481823@u.nus.edu>
2026-01-30 16:00:26 +0800

    Add to List

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
